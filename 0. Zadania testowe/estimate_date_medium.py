90dc0f9c165c7ae77c813647443de6a8991df8d1
lolekgk <karol.gajda97@gmail.com>
2022-07-14 13:50:05 +0200

    Add task solution

---
+++ 0. Zadania testowe/estimate_date_medium.py
@@ -0,0 +1,11 @@
+import math
+from datetime import date, datetime, timedelta
+
+WORK_DAY_HOURS = 8
+
+
+def estimate_date(start_date, estimated_hours):
+    start_date = date.fromisoformat(start_date)
+    work_days = math.ceil(estimated_hours / WORK_DAY_HOURS)
+    end_time = start_date + timedelta(work_days)
+    return work_days, end_time
