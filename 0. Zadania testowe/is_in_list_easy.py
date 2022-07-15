fac65cd034446b9903ef3f1657d2597c4a275726
lolekgk <karol.gajda97@gmail.com>
2022-07-15 14:02:22 +0200

    Fix bug - using in operator was not allowed

--- 0. Zadania testowe/is_in_list_easy.py
+++ 0. Zadania testowe/is_in_list_easy.py
@@ -1,8 +1,7 @@
 def is_in_list(list, element_to_find):
     """Check wheater element is in list without in operator"""
-    for item in list:
-        if item == element_to_find:
-            return True
+    if list.count(element_to_find) > 0:
+        return True
     return False
 
 
