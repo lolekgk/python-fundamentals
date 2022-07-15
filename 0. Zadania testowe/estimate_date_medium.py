31d6848781ad75c839826cd59a6b0f16370ec7d0
lolekgk <karol.gajda97@gmail.com>
2022-07-15 10:15:46 +0200

    Add final task solution

--- 0. Zadania testowe/estimate_date_medium.py
+++ 0. Zadania testowe/estimate_date_medium.py
@@ -1,20 +1,19 @@
 import math
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 
 WORKING_HOURS = 8
 
 
+# I assume that user provide date in iso format or just use today's date
 def estimate_date(start_date, estimated_hours):
-    # start_date = date.fromisoformat(start_date)
-    start_date = datetime.strptime(start_date, "%d/%m/%Y")
+    start_date = date.fromisoformat(start_date)
     work_days = math.ceil(estimated_hours / WORKING_HOURS)
-    task_end_date = (start_date + timedelta(work_days)).strftime("%d/%m/%Y")
+    task_end_date = (start_date + timedelta(work_days)).strftime("%d-%m-%Y")
     return work_days, task_end_date
 
 
 if __name__ == '__main__':
-    # work_days, task_end_date = estimate_date(str(date.today()), 10)
-    work_days, task_end_date = estimate_date("15/07/2022", 10)
+    work_days, task_end_date = estimate_date(str(date.today()), 10)
     print(f'Work days needed to finish the task: {work_days}.')
     print(f'Estimated end date of task: {task_end_date}.')
 
