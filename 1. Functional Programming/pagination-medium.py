be4ee4dbda063dafa52705d934ae339c676536d2
lolekgk <karol.gajda97@gmail.com>
2022-07-18 15:43:49 +0200

    Fix is_valid_page function

--- 1. Functional Programming/pagination-medium.py
+++ 1. Functional Programming/pagination-medium.py
@@ -5,12 +5,14 @@ def paginate(items, max_elements, page_number):
 
 
 def is_valid_page(page_number):
-    if page_number < 1:
-        raise TypeError('Page number must be positive.')
+    if not isinstance(page_number, int):
+        raise TypeError('Page number must be an int type.')
+    elif page_number < 1:
+        raise ValueError('Page number must be positive.')
     return True
 
 
-print(paginate(list(range(101)), 10, 0))
+print(paginate(list(range(101)), 10, 1))
 
 # We can also use zip_longest from itertools, but it'll fill rest of page with fillvalue
 # Or more-itertools library -> chunked method
