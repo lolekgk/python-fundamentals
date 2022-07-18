2f288b30e8873c7ed80d62de474f3d3f22ff3a25
lolekgk <karol.gajda97@gmail.com>
2022-07-18 18:53:29 +0200

    Fix random_number function

--- 1. Functional Programming/infinite_randoms-medium.py
+++ 1. Functional Programming/infinite_randoms-medium.py
@@ -4,15 +4,16 @@ history = []
 
 
 def random_number():
+    global history
     while True:
-        yield random()
+        number = random()
+        history.append(number)
+        yield number
 
 
 gen = random_number()
 
 for _ in range(10):
     num = next(gen)
-    print(num)
-    history.append(num)
 
 print(history)
