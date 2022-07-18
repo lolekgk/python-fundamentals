0e455ba64769b127b5d7e54429be03f80e43a70a
lolekgk <karol.gajda97@gmail.com>
2022-07-18 19:28:33 +0200

    Fix normalize_data function -> not sure if it works properly

--- 1. Functional Programming/normalize-medium.py
+++ 1. Functional Programming/normalize-medium.py
@@ -9,21 +9,24 @@ data = [random.randint(0, 100000) for x in range(100)]
 def random_number(data):
     """Normalizes given list of integers to range <0, 1>"""
     smallest_num, biggest_num = min(data), max(data)
-    return (
+    return [
         (num - smallest_num) / (biggest_num - smallest_num) for num in data
-    )
+    ]
 
 
 # https://stats.stackexchange.com/questions/380276/how-to-normalize-data-between-0-and-1
 def normalize_data(data):
     """Normalizes given list of integers to range <0, 1)"""
-    standard_score_data = [
-        (num - statistics.mean(data)) / statistics.stdev(data) for num in data
+    epsilon = 0.01
+    return [
+        (epsilon + (1 - 2 * epsilon))
+        * (num - min(data))
+        / (max(data) - min(data))
+        for num in data
     ]
-    return (1 / (1 + math.exp(-num)) for num in standard_score_data)
 
 
-pprint(list(random_number(data)))
-
-
-a = list(normalize_data(data))
+# pprint(random_number(data))
+# pprint(normalize_data(data))
+print(statistics.mean(normalize_data(data)))
+print(statistics.mean(random_number(data)))
