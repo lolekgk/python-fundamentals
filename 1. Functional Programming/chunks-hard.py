9b0e258836e37d75c1139ba3c0a275186189526a
lolekgk <karol.gajda97@gmail.com>
2022-07-20 11:11:37 +0200

    Add alternative solution - faster

--- 1. Functional Programming/chunks-hard.py
+++ 1. Functional Programming/chunks-hard.py
@@ -1,5 +1,6 @@
-from itertools import islice
-from random import randint
+import math
+from itertools import combinations_with_replacement, islice
+from random import choice, randint
 
 alphabet = [x for x in "abcdefghijklmnoprstuwxyz"]
 
@@ -17,3 +18,22 @@ def to_chunks(data, min_chunk=4, max_chunk=7):
 
 chunks = to_chunks(alphabet)
 print(chunks)
+
+
+def to_chunks2(data, min_=4, max_=7):
+    max_chunk_length = int(len(data) / min_)
+    min_chunk_length = math.ceil(len(data) / max_)
+    chunks_lengths = [
+        el
+        for el in combinations_with_replacement(
+            range(min_, max_ + 1),
+            randint(min_chunk_length, max_chunk_length),
+        )
+        if sum(el) == 24
+    ]
+    it = iter(data)
+    return [list(islice(it, item)) for item in choice(chunks_lengths)]
+
+
+chunks2 = to_chunks2(alphabet)
+print(chunks2)
