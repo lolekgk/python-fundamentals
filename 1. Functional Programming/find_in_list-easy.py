c60317af606de638c8a4fbfa93e6a69957fa5e5f
lolekgk <karol.gajda97@gmail.com>
2022-07-21 13:15:21 +0200

    Change find_item function
    Right know function can return more than one namedtuple.

--- 1. Functional Programming/find_in_list-easy.py
+++ 1. Functional Programming/find_in_list-easy.py
@@ -1,13 +1,13 @@
 from collections import namedtuple
 
 
-def find_item(list_of_items, key):
-    FoundItem = namedtuple('Result', ['value', 'index'])
-    for i, item in enumerate(list_of_items):
-        if str(key) in str(item):
-            return FoundItem(item, i)
-    return ()
+def find_item(data, key):
+    Result = namedtuple('Result', ['value', 'index'])
+    results_list = [
+        Result(item, i) for i, item in enumerate(data) if str(key) in str(item)
+    ]
+    return results_list if results_list else ()
 
 
-result = find_item(['nine', 'ten', 'eleven'], 'el')
+result = find_item(['nine', 'ten', 'eleven', 'twelve'], 'el')
 print(result)
