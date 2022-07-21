f5facdcc246e714d94e3bb2d9b56b64f716725c8
lolekgk <karol.gajda97@gmail.com>
2022-07-21 13:57:00 +0200

    Refactor code

--- 1. Functional Programming/find_in_list-easy.py
+++ 1. Functional Programming/find_in_list-easy.py
@@ -2,11 +2,11 @@ from collections import namedtuple
 
 
 def find_item(data, key):
-    Result = namedtuple('Result', ['value', 'index'])
-    results_list = [
-        Result(item, i) for i, item in enumerate(data) if str(key) in str(item)
+    Item = namedtuple('Result', ['value', 'index'])
+    items_list = [
+        Item(item, i) for i, item in enumerate(data) if str(key) in str(item)
     ]
-    return results_list if results_list else ()
+    return items_list if items_list else ()
 
 
 result = find_item(['nine', 'ten', 'eleven', 'twelve'], 'el')
