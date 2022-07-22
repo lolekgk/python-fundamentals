c2b6cc030a3d47e86f7cc2e3099e22ddd4ad2f3a
lolekgk <karol.gajda97@gmail.com>
2022-07-22 12:40:37 +0200

    Refactor code

--- 0. Zadania testowe/estimate_date_medium.py
+++ 0. Zadania testowe/estimate_date_medium.py
@@ -8,18 +8,17 @@ def estimate_date(start_date, estimated_hours):
     work_days = math.ceil(estimated_hours / 8)
     holidays_data = holidays.PL(years=start_date.year).keys()
     days_to_add = work_days
-    current_date = start_date
+    end_date = start_date
 
     while days_to_add:
-        weekday = current_date.isoweekday()
-        current_date += timedelta(days=1)
-        if current_date.year != start_date.year:
-            holidays_data = holidays.PL(years=current_date.year).keys()
-        if weekday > 5 or current_date in holidays_data:
+        end_date += timedelta(days=1)
+        if end_date.year != start_date.year:
+            holidays_data = holidays.PL(years=end_date.year).keys()
+        if end_date.isoweekday() > 5 or end_date in holidays_data:
             continue
         days_to_add -= 1
 
-    return work_days, current_date
+    return work_days, end_date
 
 
 start_date = date(2022, 10, 31)
