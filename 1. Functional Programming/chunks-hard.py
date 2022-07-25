aec322837974044ae5c5414b5daf90a0eb5ee1ad
lolekgk <karol.gajda97@gmail.com>
2022-07-25 16:51:47 +0200

    Fix to_chunks function after CR

--- 1. Functional Programming/chunks-hard.py
+++ 1. Functional Programming/chunks-hard.py
@@ -1,39 +1,19 @@
-import math
-from itertools import combinations_with_replacement, islice
-from random import choice, randint
+from random import randint
 
 alphabet = [x for x in "abcdefghijklmnoprstuwxyz"]
 
 
-def to_chunks(data, min_chunk=4, max_chunk=7):
-    chunks_lengths = []
-    while sum(chunks_lengths) != len(data):
-        chunks_lengths.append(randint(min_chunk, max_chunk))
-        if sum(chunks_lengths) > len(data):
-            chunks_lengths.clear()
-
-    it = iter(data)
-    return [list(islice(it, elem)) for elem in chunks_lengths]
-
-
-chunks = to_chunks(alphabet)
-print(chunks)
-
-
-def to_chunks2(data, min_=4, max_=7):
-    max_chunk_length = int(len(data) / min_)
-    min_chunk_length = math.ceil(len(data) / max_)
-    chunks_lengths = [
-        el
-        for el in combinations_with_replacement(
-            range(min_, max_ + 1),
-            randint(min_chunk_length, max_chunk_length),
-        )
-        if sum(el) == 24
-    ]
-    it = iter(data)
-    return [list(islice(it, item)) for item in choice(chunks_lengths)]
-
-
-chunks2 = to_chunks2(alphabet)
-print(chunks2)
+def to_chunks(data, min_=4, max_=7):
+    if len(data) <= max_:
+        return [data]
+    elif len(data) <= max_ * 2:
+        if len(data) > min_ + max_:
+            chunk_length = randint(len(data) - max_, max_)
+        else:
+            chunk_length = randint(min_, len(data) - min_)
+        return [data[:chunk_length], data[chunk_length:]]
+    else:
+        chunk_length = randint(min_, max_)
+        divided_data = [data[:chunk_length]]
+        divided_data.extend(to_chunks(data[chunk_length:], min_, max_))
+        return divided_data
