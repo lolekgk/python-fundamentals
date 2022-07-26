6ec3070c7cdff2aef816100ca5fea5c096a1d69f
lolekgk <karol.gajda97@gmail.com>
2022-07-26 21:06:09 +0200

    Refactor code

--- 1. Functional Programming/pagination.py
+++ 1. Functional Programming/pagination.py
@@ -1,13 +1,4 @@
 def paginate(items, max_elements, page_number):
-    if is_valid_page(page_number):
-        page_start = (page_number - 1) * max_elements
-        page_end = page_start + max_elements
-        return items[page_start:page_end]
-
-
-def is_valid_page(page_number):
-    if not isinstance(page_number, int):
-        raise TypeError('Page number must be an int type.')
-    elif page_number < 1:
-        raise ValueError('Page number must be positive.')
-    return True
+    page_start = (page_number - 1) * max_elements
+    page_end = page_start + max_elements
+    return items[page_start:page_end]
