d3449e2b01fae0e7fd39b167da44c10eb831d3b3
lolekgk <karol.gajda97@gmail.com>
2022-07-27 21:22:22 +0200

    Fix typo and code refactor

--- 1. Functional Programming/power_of_colors.py
+++ 1. Functional Programming/power_of_colors.py
@@ -5,9 +5,9 @@ def random_hex_colors():
     return f'#{random.randbytes(3).hex()}'
 
 
-def random_rbg_colors():
+def random_rgb_colors():
     return random.randrange(255), random.randrange(255), random.randrange(255)
 
 
 print(random_hex_colors())
-print(random_rbg_colors())
+print(random_rgb_colors())
