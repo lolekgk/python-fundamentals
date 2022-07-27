ec2a2afede5c5dc1a91701507569c5c104541827
lolekgk <karol.gajda97@gmail.com>
2022-07-27 21:23:34 +0200

    Refactor code

--- 1. Functional Programming/power_of_colors.py
+++ 1. Functional Programming/power_of_colors.py
@@ -7,7 +7,3 @@ def random_hex_colors():
 
 def random_rgb_colors():
     return random.randrange(255), random.randrange(255), random.randrange(255)
-
-
-print(random_hex_colors())
-print(random_rgb_colors())
