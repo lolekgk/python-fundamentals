e2d926ee58036bf8927f7b2a4edec11beef1040f
lolekgk <karol.gajda97@gmail.com>
2022-08-22 14:58:29 +0200

    Reverting to db3341a82dfeb1b8b78105672cba39288ebcbacb

--- 2. Object-oriented programming/flight_list.py
+++ 2. Object-oriented programming/flight_list.py
@@ -1,38 +1,31 @@
 import uuid
+from dataclasses import dataclass, field
 from datetime import datetime
 
-import attr
 
-
-@attr.s(slots=True, on_setattr=attr.setters)
+@dataclass(slots=True)
 class Airplane:
-    _flights = []
+    flight_number: int
+    destination: str
+    origin: str
+    start_time: datetime = field(default=None)
+    landing_time: datetime = field(default=None)
+    _flight_id: uuid = field(
+        default_factory=uuid.uuid4, init=False, repr=False
+    )
+    _flights: list = field(default_factory=list, init=False, repr=False)
+    _status: str = field(default='on the ground', init=False, repr=False)
 
-    flight_number: int = attr.ib(
-        validator=attr.validators.instance_of(int),
-        on_setattr=attr.setters.validate,
-    )
-    destination: str = attr.ib(
-        validator=attr.validators.instance_of(str),
-        on_setattr=attr.setters.validate,
-    )
-    origin: str = attr.ib(
-        validator=attr.validators.instance_of(str),
-        on_setattr=attr.setters.validate,
-    )
-    _start_time: datetime = attr.ib(default=None)
-    landing_time: datetime = attr.ib(default=None)
-    _flight_id: uuid = attr.ib(default=None, init=False, repr=False)
-    status: str = attr.ib(default='on the ground', init=False, repr=False)
-
-    def __attrs_post_init__(self):
-        if self.flight_number in [cls.flight_number for cls in self._flights]:
-            raise ValueError('You cannot add the same flight!')
+    def __post_init__(self):
         self._flights.append(self)
 
     @property
     def flight_id(self):
         return self._flight_id
+
+    @property
+    def flights(self):
+        return self._flights
 
     @property
     def status(self):
@@ -45,20 +38,14 @@ class Airplane:
             self._status = 'on the ground'
         return self._status
 
-    @property
-    def start_time(self):
-        return self._start_time
+    @status.setter
+    def status(self, status: str):
+        if not isinstance(status, str):
+            raise TypeError(f'The value of "" should be of type str.')
 
-    @start_time.setter
-    def start_time(self, start_time: datetime):
-        if start_time:
-            if isinstance(start_time, datetime):
-                self._start_time = start_time
-        self._start_time = start_time
-
-    def calculate_flight_time(self):
+    def flight_time(self):
         return self.landing_time - self.start_time
 
     @classmethod
     def list_all_flights(cls):
-        return cls._flights
+        return cls.flights
