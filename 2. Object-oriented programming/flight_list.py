ac5dc092fe52f2060806e232026b98558fc10e77
lolekgk <karol.gajda97@gmail.com>
2022-08-22 16:17:03 +0200

    Fix logic error in status property and refactor code

--- 2. Object-oriented programming/flight_list.py
+++ 2. Object-oriented programming/flight_list.py
@@ -30,31 +30,28 @@ class Airplane:
         return self._flight_id
 
     @property
-    def flights(self):
-        return self._flights
-
-    @property
-    def status(self):
-        if (
-            self.start_time < datetime.now()
-            and self.landing_time > datetime.now()
-        ):
-            self._status = 'in the air'
-        else:
-            self._status = 'on the ground'
+    def status(self) -> str:
+        if self.start_time and self.landing_time is not None:
+            if (
+                self.start_time < datetime.now()
+                and self.landing_time > datetime.now()
+            ):
+                self._status = 'in the air'
+            else:
+                self._status = 'on the ground'
         return self._status
 
     @property
-    def _flight_number(self):
+    def _flight_number(self) -> int:
         return self._flight_number
 
     @property
     def _flight_id(self):
         return self._flight_id
 
-    def flight_time(self):
+    def flight_time(self) -> datetime:
         return self.landing_time - self.start_time
 
     @classmethod
-    def list_all_flights(cls):
+    def list_all_flights(cls) -> list:
         return cls._flights
