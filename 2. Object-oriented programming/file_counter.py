3725c4f1653981930eac48783b81af86ed67d2f1
lolekgk <karol.gajda97@gmail.com>
2022-08-23 15:11:31 +0200

    Finish all tasks

---
+++ 2. Object-oriented programming/file_counter.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+class FileCounter:
+    def __init__(self):
+        self._files_count = 0
+        self._dirs_count = 0
+
+    def __reset(self):
+        "Reset instance attributes to init values"
+        self._files_count = 0
+        self._dirs_count = 0
+
+    def _create_files_tree(self, path: Path) -> dict:
+        dirs, files = [], []
+        for item in path.iterdir():
+            if item.is_dir():
+                self._dirs_count += 1
+                dirs.append(self._create_files_tree(item))
+            else:
+                self._files_count += 1
+                files.append(item.name)
+        return {
+            'dir_name': path.name,
+            'dirs': dirs,
+            'files': files,
+        }
+
+    def file_counter(self, path: Path) -> dict:
+        self.__reset()
+        searched_tree = self._create_files_tree(path)
+        return {
+            'files': self._files_count,
+            'folders': self._dirs_count,
+            'results': searched_tree,
+        }
