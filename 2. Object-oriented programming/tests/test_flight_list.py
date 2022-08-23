630d4044068b3205f10f84c29dea6bc25b4e8ba8
lolekgk <karol.gajda97@gmail.com>
2022-08-23 19:55:59 +0200

    Fix tests by adding teardowns

--- 2. Object-oriented programming/tests/test_flight_list.py
+++ 2. Object-oriented programming/tests/test_flight_list.py
@@ -26,52 +26,77 @@ def fake_starting_time():
 
 @pytest.fixture
 def airplane(start_time, landing_time):
-    return Airplane(815, 'Berlin', 'Warsaw', start_time, landing_time)
+    airplane = Airplane(815, 'Berlin', 'Warsaw', start_time, landing_time)
+    yield airplane
+    Airplane._flights.remove(airplane)
+    del airplane
+
+
+@pytest.fixture
+def airplane_without_start_time(landing_time):
+    airplane = Airplane(815, 'Berlin', 'Warsaw', landing_time=landing_time)
+    yield airplane
+    Airplane._flights.remove(airplane)
+    del airplane
+
+
+@pytest.fixture
+def airplane_without_landing_time(start_time):
+    airplane = Airplane(815, 'Berlin', 'Warsaw', start_time)
+    yield airplane
+    Airplane._flights.remove(airplane)
+    del airplane
+
+
+@pytest.fixture
+def flying_airplane(fake_starting_time, fake_landing_time):
+    airplane = Airplane(
+        113, 'Madrid', 'Wroclaw', fake_starting_time, fake_landing_time
+    )
+    yield airplane
+    Airplane._flights.remove(airplane)
+    del airplane
+
+
+@pytest.fixture
+def airplane_without_dates():
+    airplane = Airplane(113, 'Madrid', 'Wroclaw')
+    yield airplane
+    Airplane._flights.remove(airplane)
+    del airplane
 
 
 class TestAirplane:
-    def test_status_without_start_time(self, landing_time):
-        airplane = Airplane(815, 'Berlin', 'Warsaw', landing_time=landing_time)
-        assert airplane.status == 'on the ground'
+    def test_status_without_start_time(self, airplane_without_start_time):
+        assert airplane_without_start_time.status == 'on the ground'
 
-    def test_status_without_landing_time(self, start_time):
-        airplane = Airplane(815, 'Berlin', 'Warsaw', start_time)
-        assert airplane.status == 'on the ground'
+    def test_status_without_landing_time(self, airplane_without_landing_time):
+        assert airplane_without_landing_time.status == 'on the ground'
 
-    def test_status_when_airplane_is_flying(
-        self, fake_starting_time, fake_landing_time
-    ):
-        airplane = Airplane(
-            113, 'Madrid', 'Wroclaw', fake_starting_time, fake_landing_time
-        )
-        assert airplane.status == 'in the air'
+    def test_status_when_airplane_is_flying(self, flying_airplane):
+        assert flying_airplane.status == 'in the air'
 
     def test_status_after_landing(self, airplane):
         assert airplane.status == 'on the ground'
 
-    def test_no_permission_for_creating_objects_with_the_same_attributes(self):
-        airplane1 = Airplane(815, 'Berlin', 'Warsaw', start_time, landing_time)
-        with pytest.raises(ValueError):
-            airplane2 = Airplane(
-                815, 'Berlin', 'Warsaw', start_time, landing_time
-            )
-
     def test_flight_time_with_provided_dates(self, airplane):
         assert airplane.flight_time() == timedelta(seconds=14400)
 
-    def test_flight_time_without_provided_dates(self):
-        airplane = Airplane(815, 'Berlin', 'Warsaw')
+    def test_flight_time_without_provided_dates(self, airplane_without_dates):
         with pytest.raises(ValueError):
-            airplane.flight_time()
+            airplane_without_dates.flight_time()
 
-    def test_list_all_flights(self):
-        airplane1 = Airplane(815, 'Berlin', 'Warsaw', start_time, landing_time)
-        airplane2 = Airplane(
-            211, 'London', 'New York', start_time, landing_time
+    def test_list_all_flights(self, airplane, flying_airplane):
+        assert len(airplane.list_all_flights()) == 2
+        assert len(airplane.list_all_flights()) == len(
+            flying_airplane.list_all_flights()
         )
-        assert len(airplane1.list_all_flights()) == 2
-        assert len(airplane1.list_all_flights()) == len(
-            airplane2.list_all_flights()
-        )
-        for flight in airplane1.list_all_flights():
+        for flight in airplane.list_all_flights():
             assert isinstance(flight, Airplane)
+
+    def test_no_permission_for_creating_objects_with_the_same_attributes(self):
+        airplane = Airplane(810, 'Berlin', 'Warsaw', start_time, landing_time)
+        with pytest.raises(ValueError):
+            airplane = Airplane(
+                810, 'Berlin', 'Warsaw', start_time, landing_time
+            )
