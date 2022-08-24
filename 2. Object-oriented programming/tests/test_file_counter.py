86f02058aa70b5dfe77661c5aee2193b177048b1
lolekgk <karol.gajda97@gmail.com>
2022-08-24 14:50:49 +0200

    Set up first tests for file_counter.py

--- 2. Object-oriented programming/tests/test_file_counter.py
+++ 2. Object-oriented programming/tests/test_file_counter.py
@@ -1,4 +1,37 @@
+from pathlib import Path
+
 import pytest
 from file_counter import FileCounter
 
-# path = Path('/Users/karolgajda/test/')
+
+@pytest.fixture
+def extensive_fake_path(fs):
+    # "fs" is the reference to the fake file system
+    fs.create_file('/test/test1/xx1.txt')
+    fs.create_file('/test/test2/xx2_1.txt')
+    fs.create_file('/test/test2/xx2_2.txt')
+    fs.create_file('/test/test2/xx2_3.txt')
+    fs.create_file('/test/test2/test2_1/xx2.txt')
+    fs.create_file('/test/test3/xx3.txt')
+    fs.create_file('/test/test3/test3_1/xx3.txt')
+    yield fs
+
+
+@pytest.fixture
+def file_counter():
+    f = FileCounter()
+    yield f
+    del f
+
+
+class TestFileCounter:
+    # fs is fixture from pyfakefs library
+
+    def test_file_counter_with_extensive_path(
+        self, extensive_fake_path, file_counter
+    ):
+        path = Path('/test')
+        result = file_counter.file_counter(path)
+        assert result['files'] == 7
+        assert result['folders'] == 5
+        assert isinstance(result['results'], dict)
