6585f9d58b59ee0abb00e217a53c6d397b3eff2e
lolekgk <karol.gajda97@gmail.com>
2022-08-30 17:07:25 +0200

    Add tests for Redactor class

--- 2. Object-oriented programming/tests/test_user_management.py
+++ 2. Object-oriented programming/tests/test_user_management.py
@@ -153,3 +153,78 @@ class TestUser:
         test_dict = {test_user: 1}
         assert test_dict[test_user] == 1
         del test_dict
+
+
+class TestRedactor:
+    def test_redactor_can_add_post(self, test_redactor, test_redactors_post):
+        assert test_redactors_post.author == test_redactor
+        assert test_redactors_post.content == 'Post content'
+        assert len(test_redactor._posts) == 1
+
+    def test_redactor_can_edit_his_own_post(
+        self, test_redactor, test_redactors_post
+    ):
+        test_redactor.edit_post(test_redactors_post, 'New content')
+        assert test_redactors_post.content == 'New content'
+
+    def test_redactor_can_edit_other_users_posts(
+        self, test_redactor, test_users_post
+    ):
+        test_redactor.edit_post(test_users_post, 'New content')
+        assert test_users_post.content == 'New content'
+
+    def test_redactor_can_delete_his_own_post(
+        self, test_redactor, test_redactors_post
+    ):
+        assert len(test_redactor._posts) == 1
+        test_redactor.delete_post(test_redactors_post)
+        assert len(test_redactor._posts) == 0
+
+    def test_redactor_can_not_delete_other_users_post(
+        self, test_redactor, test_users_post
+    ):
+        with pytest.raises(PermissionError):
+            test_redactor.delete_post(test_users_post)
+
+    def test_redactor_can_change_his_own_email(self, test_redactor):
+        test_redactor.change_attribute(
+            'email', 'new.email@gmail.com', test_redactor
+        )
+        assert test_redactor.email == 'new.email@gmail.com'
+
+    def test_redactor_can_not_change_name(self, test_redactor):
+        with pytest.raises(PermissionError):
+            test_redactor.change_attribute('name', 'Bob', test_redactor)
+
+    def test_redactor_can_not_change_own_surnname(self, test_redactor):
+        with pytest.raises(PermissionError):
+            test_redactor.change_attribute('surname', 'Marley', test_redactor)
+
+    def test_redactor_can_not_change_own_birth_date(self, test_redactor):
+        with pytest.raises(PermissionError):
+            test_redactor.change_attribute(
+                'birth_date', date(2020, 1, 2), test_redactor
+            )
+
+    def test_redactor_can_not_change_own_gender(self, test_redactor):
+        with pytest.raises(PermissionError):
+            test_redactor.change_attribute('gender', 'female', test_redactor)
+
+    def test_redactor_can_not_set_own_new_attribute(self, test_redactor):
+        with pytest.raises(PermissionError):
+            test_redactor.change_attribute(
+                'second_name', 'Francis', test_redactor
+            )
+
+    def test_redactor_can_not_change_others_attributes(
+        self, test_redactor, second_test_user
+    ):
+        with pytest.raises(PermissionError):
+            test_redactor.change_attribute(
+                'gender', 'female', second_test_user
+            )
+
+    def test_redactor_class_can_be_dict_key(self, test_redactor):
+        test_dict = {test_redactor: 1}
+        assert test_dict[test_redactor] == 1
+        del test_dict
