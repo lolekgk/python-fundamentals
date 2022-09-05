98764f193453e4148ec718862c62b1ef0c81bf60
lolekgk <karol.gajda97@gmail.com>
2022-09-05 17:54:56 +0200

    Add methods for reading, writing, updating and deleting json files

--- 3. Python OS/json_manager.py
+++ 3. Python OS/json_manager.py
@@ -1,4 +1,18 @@
 import json
+import os
+from pathlib import Path
+
+from pathvalidate import validate_filename
+
+
+class PathError(Exception):
+    def __init__(
+        self,
+        msg='You need to provide valid path with .json file.',
+        *args,
+        **kwargs,
+    ):
+        super().__init__(msg, *args, **kwargs)
 
 
 class Singleton(type):
@@ -12,28 +26,47 @@ class Singleton(type):
 
 
 class JsonManager(metaclass=Singleton):
-    def __init__(self, path=None):
+    _allowed_extensions = ['.json']
+
+    def __init__(self, path: Path = None):
         self.path = path
 
-    def read_file(self, path=None):
-        # json.load() -> load data from a file
-        with open(path) as json_file:
-            self.data = json.load(json_file)
+    def read(self, file_path: Path) -> dict:
+        self._path_validation(file_path)
+        with open(file_path) as json_file:
+            return json.load(json_file)
 
-    def create_file(self, data: dict):
-        # json.dump() -> write data to a file
-        with open(self.path, 'w') as json_file:
-            json.dump(data, json_file)
+    def write(self, data: dict, path: Path, update=None):
+        if update is None:
+            validate_filename(path.stem)
+            self._suffix_validation(path)
 
-    def update_file(self, path):
-        # json.load()
-        # json.dump()
-        pass
+        with open(path, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+        return self
 
-    def delete_file(self, path):
-        pass
+    def update_file(self, data: dict, file_path: Path):
+        self.read(file_path)
+        self.write(data, file_path, update=True)
+        return self
 
+    def delete_file(self, file_path: Path):
+        self._path_validation(file_path)
+        try:
+            os.remove(file_path)
+        except OSError as er:
+            print(er)
+        return self
 
-# jsonstring = '{"name": "erik", "age": 38, "married": true}'
-# data = json.loads(jsonstring) -> decoding JSON -> json to python, dict
-# json.dumps(data, indent=4) -> encoding JSON -> python to json
+    def _path_validation(self, file_path: Path):
+        if not (
+            isinstance(file_path, Path)
+            and file_path.exists()
+            and file_path.is_file()
+            and file_path.suffix in JsonManager._allowed_extensions
+        ):
+            raise PathError
+
+    def _suffix_validation(self, file_path: Path):
+        if not file_path.suffix in JsonManager._allowed_extensions:
+            raise PathError
