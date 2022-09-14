aceac49bd33dbe86006bc799244e9029789671fc
Karol Gajda <karol.gajda97@gmail.com>
2022-09-14 17:49:07 +0200

    Refactor code after CR
    - implement Tree class instead of using dict
    - added check_path decorator to get rid of code repetition
    - refactored path_to_tree, create_folder_tree and _is_valid_tree methods

--- 3. Python OS/folder_manager.py
+++ 3. Python OS/folder_manager.py
@@ -1,28 +1,49 @@
+from __future__ import annotations
+
+import inspect
 import os
 import shutil
 from pathlib import Path
 
+from pydantic import Field, StrictStr
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class Tree:
+    folder_name: StrictStr
+    content: list[Tree] = Field(default_factory=list)
+
 
 class FolderManager:
-    def __init__(self, path: Path = None):
+    def __init__(self, path: Path | None = None):
         self.path = path
 
-    def create_folder(self, path: Path = None):
-        if path is None:
-            path = self.path
+    def _check_path(func):
+        def wrapper(self, *args, **kwargs):
+
+            bound_args = inspect.signature(func).bind(self, *args, **kwargs)
+            bound_args.apply_defaults()
+            func_args = dict(bound_args.arguments)
+            if func_args['path'] is None:
+                func_args['path'] = self.path
+                return func(*func_args.values())
+            return func(*func_args.values())
+
+        return wrapper
+
+    @_check_path
+    def create_folder(self, path: Path | None = None):
         os.makedirs(path, exist_ok=True)
 
-    def list_content(self, path: Path = None) -> list:
-        if path is None:
-            path = self.path
+    @_check_path
+    def list_content(self, path: Path | None = None) -> list:
         return os.listdir(path)
 
+    @_check_path
     def delete_folder(
-        self, path: Path = None, ignore_dir_content: bool = False
+        self, path: Path | None = None, ignore_dir_content: bool = False
     ):
-        if path is None:
-            path = self.path
-
         if len(os.listdir(path)) != 0 and not ignore_dir_content:
             raise OSError(f'Directory in provided path: {path} is not empty!')
 
@@ -31,40 +52,22 @@ class FolderManager:
         except OSError as error:
             print(error)
 
-    def create_folder_tree(self, path: Path, tree: dict):
+    def create_folder_tree(self, path: Path, tree: Tree):
         """Create folder tree in your OS based on provided tree argument of
-        dictionary type.
-        Example tree structure:
-        example_tree = {'name': 'test2', 'type': 'folder', 'content': [{}, {}]}"""
+        Tree type."""
         self._is_valid_tree(tree)
-        if tree['type'] == 'folder':
-            path = path / tree['name']
-        if isinstance(tree.get('content'), list):
-            for item in tree['content']:
-                if item['type'] == 'folder':
-                    self.create_folder_tree(path, item)
+        path = path / tree.folder_name
+        for item in tree.content:
+            self.create_folder_tree(path, item)
         self.create_folder(path)
 
-    def path_to_dict(self, path: Path) -> dict:
-        """Create dict representing directory structure in given path"""
-        tree = {'name': path.name}
+    def path_to_tree(self, path: Path) -> dict:
+        """Create Tree object representing directory structure of given path"""
+        tree = Tree(path.folder_name)
         if path.is_dir():
-            tree['type'] = 'folder'
-            tree['content'] = [
-                self.path_to_dict(item) for item in path.iterdir()
-            ]
-        if path.is_file():
-            tree['type'] = 'file'
+            tree.content = [self.path_to_tree(item) for item in path.iterdir()]
         return tree
 
     def _is_valid_tree(self, tree):
-        if not isinstance(tree, dict):
-            raise TypeError('You need to provide tree of dict type.')
-        if not all(
-            item in tree.keys() for item in ['name', 'type', 'content']
-        ):
-            raise ValueError('You need to provide valid tree structure.')
-        if tree.get('content'):
-            for item in tree['content']:
-                if item.get('type') == 'folder':
-                    self._is_valid_tree(item)
+        if not isinstance(tree, Tree):
+            raise TypeError('You need to provide tree of Tree type.')
