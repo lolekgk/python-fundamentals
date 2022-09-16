99611fcb18949c1ab93e01c49b30cb3915a3226d
Karol Gajda <karol.gajda97@gmail.com>
2022-09-16 12:26:40 +0200

    Fix typo

--- 0. Zadania testowe/is_in_list_easy.py
+++ 0. Zadania testowe/is_in_list_easy.py
@@ -1,5 +1,5 @@
-def is_in_list(list, element_to_find):
+def is_in_list(list_, element_to_find):
     """Check wheater element is in list without in operator"""
-    if list.count(element_to_find) > 0:
+    if list_.count(element_to_find) > 0:
         return True
     return False
