66a0db78af4324f38b53d439517654278f703a5c
Karol Gajda <karol.gajda97@gmail.com>
2022-09-16 12:28:13 +0200

    Refactor scan_folder function, add check_path decorator

--- 3. Python OS/csv_manager.py
+++ 3. Python OS/csv_manager.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import csv
+import inspect
 import os
 from pathlib import Path
 from typing import Generator
@@ -34,68 +35,74 @@ class CSVManager(metaclass=Singleton):
     def __init__(self, path: Path = None):
         self.path = path
 
-    def read(self, path: Path = None):
-        if path is None:
-            path = self.path
+    def _check_path(func):
+        def wrapper(self, *args, **kwargs):
+
+            bound_args = inspect.signature(func).bind(self, *args, **kwargs)
+            bound_args.apply_defaults()
+            func_args = dict(bound_args.arguments)
+            if func_args['path'] is None:
+                func_args['path'] = self.path
+                return func(*func_args.values())
+            return func(*func_args.values())
+
+        return wrapper
+
+    @_check_path
+    def read(self, path: Path | None = None):
         self._is_valid_csv_file_path(path)
         with open(path, newline='', encoding='unicode_escape') as csvfile:
             reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
             for row in reader:
                 print(' '.join(row))
 
+    @_check_path
     def write(
         self,
         rows: list[dict],
-        header: list[str] = None,
-        path: Path = None,
-        update: bool = None,
+        header: list[str] | None = None,
+        path: Path | None = None,
+        update: bool = False,
     ) -> CSVManager:
         """Write to .csv file, create if it is not exist."""
-        if path is None:
-            path = self.path
-
-        if update is None:
+        if not update:
             self._is_valid_csv_suffix(path)
             validate_filename(path.stem)
 
         with open(path, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            if header is not None:
+            if header:
                 writer.writerow(header)
             for row in rows:
                 if isinstance(row, dict):
                     writer.writerow(list(row.values()))
-        return self
 
-    def delete_file(self, path: Path = None) -> CSVManager:
-        if path is None:
-            path = self.path
+    @_check_path
+    def delete_file(self, path: Path | None = None) -> CSVManager:
         self._is_valid_csv_file_path(path)
         try:
             os.remove(path)
         except OSError as err:
             print(err)
-        return self
 
-    def update_file(self, data: list[dict], path: Path = None) -> CSVManager:
-        if path is None:
-            path = self.path
+    @_check_path
+    def update_file(
+        self, data: list[dict], path: Path | None = None
+    ) -> CSVManager:
         self.read(path)
         self.write(rows=data, path=path, update=True)
-        return self
 
-    def scan_path(self, path: Path = None, depth: int = None) -> Generator:
+    def scan_folder(
+        self, path: Path | None = None, depth: int = -1
+    ) -> Generator:
         """Recursively list files ending with .csv suffix in all folders in given location
         or up to a certain depth - if provided"""
-        if path is None:
-            path = self.path
-
-        if depth is None:
+        self._is_valid_folder_path(path)
+        if depth < 0:
             for csv_path in path.rglob(f"*{CSVManager._allowed_extension}"):
                 yield csv_path
 
-        if depth is not None:
-            depth -= 1
+        if depth >= 0:
             for child in path.iterdir():
                 if (
                     child.is_file()
@@ -103,23 +110,29 @@ class CSVManager(metaclass=Singleton):
                 ):
                     yield child
                 if child.is_dir() and depth > 0:
-                    yield from self.scan_path(child, depth)
+                    yield from self.scan_folder(child, depth - 1)
 
-    def _is_valid_csv_file_path(self, file_path: Path):
+    def _is_valid_csv_file_path(self, path: Path):
         if not (
-            isinstance(file_path, Path)
-            and file_path.exists()
-            and file_path.is_file()
-            and file_path.suffix == CSVManager._allowed_extension
+            isinstance(path, Path)
+            and path.exists()
+            and path.is_file()
+            and path.suffix == CSVManager._allowed_extension
         ):
             raise PathError
 
-    def _is_valid_csv_suffix(self, file_path: Path):
+    def _is_valid_csv_suffix(self, path: Path):
         if (
-            not isinstance(file_path, Path)
-            or not file_path.suffix == CSVManager._allowed_extension
+            not isinstance(path, Path)
+            or not path.suffix == CSVManager._allowed_extension
         ):
             raise PathError
+
+    def _is_valid_folder_path(self, path: Path):
+        if not isinstance(path, Path) or not path.is_dir():
+            raise PathError(
+                'You need to provide valid directory path to perform this action.'
+            )
 
     @property
     def allowed_extension(self):
