6ab3b990bbcd34e9cc669d323002f206e9886c85
Karol Gajda <karol.gajda97@gmail.com>
2022-09-23 12:53:58 +0200

    Add new setting default archive_format method

--- 3. Python OS/archive.py
+++ 3. Python OS/archive.py
@@ -7,33 +7,37 @@ from pathlib import Path
 from py7zr import pack_7zarchive, unpack_7zarchive
 
 
+def get_os():
+    return os_name
+
+
 class ArchiveFormat(Enum):
     WINDOWS = '7zip'
     POSIX = 'gztar'
 
 
 class ArchiveMixin:
-    if os_name == 'nt':
-        _archive_format = ArchiveFormat.WINDOWS.value
-        shutil.register_archive_format(
-            ArchiveFormat.WINDOWS.value,
-            pack_7zarchive,
-            description='7zip archive',
-        )
-        shutil.register_unpack_format(
-            ArchiveFormat.WINDOWS.value, ['.7z'], unpack_7zarchive
-        )
-    else:
-        _archive_format = ArchiveFormat.POSIX.value
+    @staticmethod
+    def _get_archive_format():
+
+        archive_format = ArchiveFormat.POSIX.value
+        if get_os() == 'nt':
+            archive_format = ArchiveFormat.WINDOWS.value
+            shutil.register_archive_format(
+                '7zip',
+                pack_7zarchive,
+                description='7zip archive',
+            )
+            shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
+        return archive_format
 
     def add_files_to_archive(
         self,
         src: Path,
         dst: Path,
         depth: int = -1,
-        archive_format=_archive_format,
+        archive_format: str = _get_archive_format(),
     ):
-
         with tempfile.TemporaryDirectory() as tmpdir:
             for file in self.scan_folder(src, depth):
                 shutil.copy2(file, tmpdir)
@@ -45,7 +49,10 @@ class ArchiveMixin:
             )
 
     def add_tree_to_archive(
-        self, src: Path, dst: Path, archive_format: str = _archive_format
+        self,
+        src: Path,
+        dst: Path,
+        archive_format: str = _get_archive_format(),
     ):
         with tempfile.TemporaryDirectory() as tmpdir:
             shutil.copytree(src, tmpdir, dirs_exist_ok=True)
