45bf2d8013eb9e8d98cb5915391814f27c853073
Karol Gajda <karol.gajda97@gmail.com>
2022-09-23 14:59:33 +0200

    Move scan folder and scan_path to utils

--- 3. Python OS/csv_manager.py
+++ 3. Python OS/csv_manager.py
@@ -1,54 +1,22 @@
 from __future__ import annotations
 
 import csv
-import inspect
 import os
 from pathlib import Path
-from typing import Generator
 
 from pathvalidate import validate_filename
 
-
-class PathError(Exception):
-    def __init__(
-        self,
-        msg='You need to provide valid path with .csv file to perform this action',
-        *args,
-        **kwargs,
-    ):
-        super().__init__(msg, *args, **kwargs)
+from archive import ArchiveMixin
+from utils import PathError, ScanFolderMixin, Singleton, check_path
 
 
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class CSVManager(metaclass=Singleton):
+class CSVManager(ArchiveMixin, ScanFolderMixin, metaclass=Singleton):
     _allowed_extension = '.csv'
 
     def __init__(self, path: Path = None):
         self.path = path
 
-    def _check_path(func):
-        def wrapper(self, *args, **kwargs):
-
-            bound_args = inspect.signature(func).bind(self, *args, **kwargs)
-            bound_args.apply_defaults()
-            func_args = dict(bound_args.arguments)
-            if func_args['path'] is None:
-                func_args['path'] = self.path
-                return func(*func_args.values())
-            return func(*func_args.values())
-
-        return wrapper
-
-    @_check_path
+    @check_path
     def read(self, path: Path = None):
         self._is_valid_csv_file_path(path)
         with open(path, newline='', encoding='unicode_escape') as csvfile:
@@ -56,7 +24,7 @@ class CSVManager(metaclass=Singleton):
             for row in reader:
                 print(' '.join(row))
 
-    @_check_path
+    @check_path
     def write(
         self,
         rows: list[dict],
@@ -77,7 +45,7 @@ class CSVManager(metaclass=Singleton):
                 if isinstance(row, dict):
                     writer.writerow(list(row.values()))
 
-    @_check_path
+    @check_path
     def delete_file(self, path: Path = None) -> CSVManager:
         self._is_valid_csv_file_path(path)
         try:
@@ -85,28 +53,10 @@ class CSVManager(metaclass=Singleton):
         except OSError as err:
             print(err)
 
-    @_check_path
+    @check_path
     def update_file(self, data: list[dict], path: Path = None) -> CSVManager:
         self.read(path)
         self.write(rows=data, path=path, update=True)
-
-    def scan_folder(self, path: Path = None, depth: int = -1) -> Generator:
-        """Recursively list files ending with .csv suffix in all folders in given location
-        or up to a certain depth - if provided"""
-        self._is_valid_folder_path(path)
-        if depth < 0:
-            for csv_path in path.rglob(f"*{CSVManager._allowed_extension}"):
-                yield csv_path
-
-        if depth >= 0:
-            for child in path.iterdir():
-                if (
-                    child.is_file()
-                    and child.suffix == CSVManager._allowed_extension
-                ):
-                    yield child
-                if child.is_dir() and depth > 0:
-                    yield from self.scan_folder(child, depth - 1)
 
     def _is_valid_csv_file_path(self, path: Path):
         if not (
--- 3. Python OS/json_manager.py
+++ 3. Python OS/json_manager.py
@@ -1,60 +1,28 @@
 from __future__ import annotations
 
-import inspect
 import json
 import os
 from pathlib import Path
-from typing import Generator
 
 from pathvalidate import validate_filename
 
-
-class PathError(Exception):
-    def __init__(
-        self,
-        msg='You need to provide valid path with .json file to perform this action.',
-        *args,
-        **kwargs,
-    ):
-        super().__init__(msg, *args, **kwargs)
+from archive import ArchiveMixin
+from utils import PathError, ScanFolderMixin, Singleton, check_path
 
 
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class JsonManager(metaclass=Singleton):
+class JsonManager(ArchiveMixin, ScanFolderMixin, metaclass=Singleton):
     _allowed_extension = '.json'
 
     def __init__(self, path: Path = None):
         self.path = path
 
-    def _check_path(func):
-        def wrapper(self, *args, **kwargs):
-
-            bound_args = inspect.signature(func).bind(self, *args, **kwargs)
-            bound_args.apply_defaults()
-            func_args = dict(bound_args.arguments)
-            if func_args['path'] is None:
-                func_args['path'] = self.path
-                return func(*func_args.values())
-            return func(*func_args.values())
-
-        return wrapper
-
-    @_check_path
+    @check_path
     def read(self, path: Path = None) -> dict:
         self._is_valid_json_file_path(path)
         with open(path) as json_file:
             return json.load(json_file)
 
-    @_check_path
+    @check_path
     def write(
         self, data: dict, path: Path = None, update: bool = False
     ) -> JsonManager:
@@ -65,36 +33,18 @@ class JsonManager(metaclass=Singleton):
         with open(path, 'w') as json_file:
             json.dump(data, json_file)
 
-    @_check_path
+    @check_path
     def update_file(self, data: dict, path: Path = None) -> JsonManager:
         self.read(path)
         self.write(data, path, update=True)
 
-    @_check_path
+    @check_path
     def delete_file(self, path: Path = None) -> JsonManager:
         self._is_valid_json_file_path(path)
         try:
             os.remove(path)
         except OSError as er:
             print(er)
-
-    def scan_folder(self, path: Path, depth: int = -1) -> Generator:
-        """Recursively list files ending with .json suffix in all folders in given location
-        or up to a certain depth - if provided"""
-        self._is_valid_folder_path(path)
-        if depth < 0:
-            for tree_path in path.rglob(f'*{JsonManager._allowed_extension}'):
-                yield tree_path
-
-        else:
-            for item in path.iterdir():
-                if (
-                    item.suffix == JsonManager._allowed_extension
-                    and item.is_file()
-                ):
-                    yield item
-                if item.is_dir() and depth > 0:
-                    yield from self.scan_folder(item, depth - 1)
 
     def _is_valid_json_file_path(self, path: Path):
         if not (
